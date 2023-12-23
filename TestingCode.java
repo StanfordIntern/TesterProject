c5e0a3b8be220b05bc8d4a5114ae0249ee23470e
Aroosh Salunkhe <aroosh205@gmail.com>
2023-12-23 17:36:31 -0500

    Revert "Added buggy code"
    This reverts commit 96c90ae23a88bd45cb06e940a69706fb2e76f2b7.

--- TestingCode.java
+++ TestingCode.java
@@ -2,6 +2,5 @@ public class TestingCode {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I made a number sorter!");
-        print (qwweer)
     }
 }
